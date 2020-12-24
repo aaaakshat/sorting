e5226296b754bf5df1aeab53cdadc2545a1591d7
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-12-24 21:34:38 +0800

    Add bubblesort

---
+++ bubblesort/bubblesort.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+def print_arr(x):
+    out = ""
+    for i in x:
+        out += str(i)
+        out += " "
+    print(out)
+
+def bubblesort_up(x):
+    n = len(x)
+    print("Bubblesort up:")
+    for i in range(n):
+        print_arr(x)
+        for j in range(i, n):
+            if (x[j] < x[i]):
+                t = x[j]
+                x[j] = x[i] 
+                x[i] = t
+    return x
+
+def bubblesort_down(x):
+    n = len(x)
+    print("Bubblesort down:")
+    for i in range(n-1, -1, -1):
+        print_arr(x)
+        for j in range(0, i):
+            if (x[j] > x[i]):
+                t = x[j]
+                x[j] = x[i] 
+                x[i] = t
+    return x
+
+# Get inp
+n = int(input())
+x = []
+for _ in range(n):
+    x.append(int(input()))
+
+x = bubblesort_down(x)
+
+# Print array
+print("Sorted Arr:")
+print_arr(x)
