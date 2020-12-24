ca0b056a877b10caebc92759f1e33d102830dd28
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-12-24 22:39:26 +0800

    Update cli text

--- selectionsort/selectionsort.py
+++ selectionsort/selectionsort.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+def print_arr(x):
+    out = ""
+    for i in x:
+        out += str(i)
+        out += " "
+    print(out)
+
 def selectionsort_up(x):
     n = len(x)
+    print("Sorting upwards:")
     for i in range(n):
         mini = i
+        print_arr(x)
         for j in range(i, n):
             if (x[j] < x[mini]):
                 mini = j
@@ -14,7 +23,9 @@ def selectionsort_up(x):
     
 def selectionsort_down(x):
     n = len(x)
+    print("Sorting down:")
     for i in range(n-1, -1, -1):
+        print_arr(x)
         maxi = i
         for j in range(0, i):
             if (x[j] > x[maxi]):
@@ -33,9 +44,5 @@ for _ in range(n):
 x = selectionsort_down(x)
 
 # Print array
-out = ""
-for i in x:
-    out += str(i)
-    out += " "
-
-print(out)
+print("Sorted Arr:")
+print_arr(x)
