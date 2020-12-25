09c2ed96583843e82d941cbc1032ec694f8e80e6
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-12-25 16:14:53 +0800

    Update bubblesort

--- weirdsort/weirdsort.py
+++ weirdsort/weirdsort.py
@@ -7,29 +7,28 @@ def print_arr(x):
         out += " "
     print(out)
 
-# Doesn't work for some reason
-def bubblesort_up(x):
+def weirdsort_up(x):
     n = len(x)
     print("Bubblesort up:")
     for i in range(n):
         print_arr(x)
-        for j in range(n-1):
-            if (x[j] > x[j+1]):
+        for j in range(i+1, n):
+            if (x[j] < x[i]):
                 t = x[j]
-                x[j] = x[j+1] 
-                x[j+1] = t
+                x[j] = x[i] 
+                x[i] = t
     return x
 
-def bubblesort_down(x):
+def weirdsort_down(x):
     n = len(x)
     print("Bubblesort down:")
     for i in range(n-1, -1, -1):
         print_arr(x)
-        for j in range(n-1, 0, -1):
-            if (x[j] < x[j-1]):
+        for j in range(0, i):
+            if (x[j] > x[i]):
                 t = x[j]
-                x[j] = x[j-1] 
-                x[j-1] = t
+                x[j] = x[i] 
+                x[i] = t
     return x
 
 # Get inp
@@ -38,7 +37,7 @@ x = []
 for _ in range(n):
     x.append(int(input()))
 
-x = bubblesort_down(x)
+x = weirdsort_down(x)
 
 # Print array
 print("Sorted Arr:")
